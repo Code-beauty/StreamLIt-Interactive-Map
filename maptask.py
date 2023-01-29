167b60fc5172a349fc8698769b7944dd8c45c060
Tayyaba Shaikh <123499093+Code-beauty@users.noreply.github.com>
2023-01-29 18:46:02 +0500

    Add files via upload

---
+++ maptask.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from shapely.geometry import Point, Polygon
+import geopandas as gpd
+import pandas as pd
+import geopy
+
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+
+street = st.sidebar.text_input("Street")
+city = st.sidebar.text_input("City")
+province = st.sidebar.text_input("Province")
+country = st.sidebar.text_input("Country")
+
+geolocator = Nominatim(user_agent="GTA Lookup")
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+location = geolocator.geocode(street+", "+city+", "+province+", "+country)
+
+
+st.header(street)
+lat = location.latitude
+lon = location.longitude
+map_data = pd.DataFrame({'lat': [lat], 'lon': [lon]})
+st.map(map_data , zoom = 12) 
+
+
+
+
